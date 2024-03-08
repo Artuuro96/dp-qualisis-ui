6f779352728a5b9768b1a6a55da02a03a0fddf43
Arturo Rodriguez <arturorodr96@gmail.com>
2024-03-07 23:58:28 -0600

    added select role

--- src/components/Staff.tsx
+++ src/components/Staff.tsx
@@ -1,15 +1,19 @@
-import { Card, CardContent, Grid, Typography } from "@mui/material"
+import { Box, Card, CardContent, Grid } from "@mui/material"
 import { useTitleContext } from "../context/TitleContext";
 import { useEffect } from "react";
+import SelectRole from "./SelectRole";
 
 export default function Staff(): JSX.Element {
   const { setTitle } = useTitleContext();
   const users = [
     {
       name: 'Arturo',
-      assignment: {
-        startDate: new Date()
-      }
+      username: 'arturo96',
+      email: 'arturorodr96@gmail.com',
+      permissions: ['create:users', 'get:users'],
+      roles: ['worker', 'metrologo', 'admin'],
+      modulos: ['tools', 'users', 'staff'],
+      active: true,
     }
   ]
   
@@ -18,19 +22,57 @@ export default function Staff(): JSX.Element {
   }, [setTitle]);
 
   return (
-    <Grid container>
-      { users.map(user => (
-        <Card sx={{ minWidth: 275 }}>
+    <Box>
+      <Card sx={{ minWidth: 275 }}>
         <CardContent>
-          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-            {user.name}
-          </Typography>
-          <Typography variant="body2">
-            { user.assignment.startDate.toDateString() }
-          </Typography>
+          <Grid container spacing={2} justifyContent="center">
+            <Grid item xs={2}>
+              Nombre
+            </Grid>
+            <Grid item xs={2}>
+              Usuario
+            </Grid>
+            <Grid item xs={2}>
+              Rol
+            </Grid>
+            <Grid item xs={2}>
+              Permisos
+            </Grid>
+            <Grid item xs={2}>
+              Modulos
+            </Grid>
+            <Grid item xs={2}>
+              Activo
+            </Grid>
+          </Grid>
         </CardContent>
       </Card>
-      )) }
-    </Grid>
+      {users.map(user => (
+        <Card sx={{ minWidth: 275 }}>
+          <CardContent>
+            <Grid container spacing={2}>
+              <Grid item xs={2}>
+                {user.name}
+              </Grid>
+              <Grid item xs={2}>
+                {user.username}
+              </Grid>
+              <Grid item xs={2}>
+                <SelectRole data={user.roles}/>
+              </Grid>
+              <Grid item xs={2}>
+                {JSON.stringify(user.permissions)}
+              </Grid>
+              <Grid item xs={2}>
+                {JSON.stringify(user.modulos)}
+              </Grid>
+              <Grid item xs={2}>
+                {user.active.toString()}
+              </Grid>
+            </Grid>
+          </CardContent>
+        </Card>
+      ))}
+    </Box>
   )
 }
